901b8f2426b4d7391d662cd606d45ced431298a0
Nprime496 <nnanejunior@gmail.com>
2019-10-22 14:59:23 +0100

    fin sqlite fonctionnne

--- thanks.php
+++ thanks.php
@@ -10,14 +10,19 @@
 	<p class="message">
 		<?php
 			try {
-				$bdd=new PDO('mysql:host=localhost;dbname:mashapp_bd','root','');
-				$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//pour pouvoir afficher les erreurs
+				$bdd=new SQLite3("mashapp_bd");
+				//$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//pour pouvoir afficher les erreurs
 				$name=$_POST["name"];
 				$surname=$_POST["surname"];
 				$language=$_POST["language"];
 				$date=$_POST["date"];
-				//changer ceci par une requête preparée : NEVER TRUST USER INPUT
-				$donnees=$bdd->query('INSERT INTO mashapp_bd.parrains(name,surname,birthday) VALUES ("'.$name.'","'.$surname.'",STR_TO_DATE("'.$date.'", "%Y-%m-%d"))');
+				//NEVER TRUST USER INPUT
+				$statement=$bdd->prepare('INSERT INTO parrains(name,surname,birthday) VALUES ( ?,?,?)');				
+				$statement->bindParam(1,$_POST['name']);
+				$statement->bindParam(2,$_POST['surname']);
+				$statement->bindParam(3,$_POST['date']);
+				$statement->execute();
+				//fin
 			} catch (Exception $e) {
 				die("ERREUR !".$e->getMessage());
 			}
