b62d92ae9c6518de01ece15e8b48d78bb4aad10e
Nprime496 <nnanejunior@gmail.com>
2019-10-23 15:52:42 +0100

    test krk

--- thanks.php
+++ thanks.php
@@ -9,7 +9,8 @@
 <body>
 	<p class="message">
 		<?php
-			try {
+			/*try
+			{
 				$bdd=new SQLite3("mashapp_bd");
 				//$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//pour pouvoir afficher les erreurs
 				$name=$_POST["name"];
@@ -23,9 +24,11 @@
 				$statement->bindParam(3,$_POST['date']);
 				$statement->execute();
 				//fin
-			} catch (Exception $e) {
-				die("ERREUR !".$e->getMessage());
 			}
+			catch (Exception $e) 
+			{
+				die("ERREUR !".$e->getMessage());
+			}*/
 		 ?>
 		Thanks for your participation  <?php echo $_POST["surname"]; ?> !!<br>
 		Your informations have been registered!
